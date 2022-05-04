12b4998531c878fe65e6d0762b7fdf9689ccb6a3
Ashutosh <bhardwajashu96@hotmail.com>
2022-05-04 14:15:16 +0530

    feat: docs added

---
+++ docs/definition.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "movie": {
+    "type": "object",
+    "properties": {
+      "userId": {
+        "type": "number"
+      },
+      "title": {
+        "type": "string"
+      },
+      "released": {
+        "type": "Date"
+      },
+      "genre": {
+        "type": "string"
+      },
+      "director": {
+        "type": "string"
+      },
+      "createdAt": {
+        "type": "Date"
+      },
+      "updatedAt": {
+        "type": "Date"
+      }
+    },
+  }
+};
